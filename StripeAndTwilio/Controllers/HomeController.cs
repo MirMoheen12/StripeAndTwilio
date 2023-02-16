55f83a94e54aa0c99e498e2b8e6d3b8c03c4a429
MirMoheen12 <91481488+MirMoheen12@users.noreply.github.com>
2023-02-16 19:06:20 +0500

    Paypal payment info

--- StripeAndTwilio/Controllers/HomeController.cs
+++ StripeAndTwilio/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Stripe.Checkout;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -18,6 +20,43 @@ namespace StripeAndTwilio.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+        [HttpGet]
+        public ActionResult CreateCheckoutSession()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCheckoutSession(string amount)
+        {
+            var options = new Stripe.Checkout.SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = Convert.ToInt32(amount) * 100,
+                            Currency = "inr",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = "T-Shirt"
+
+                            },
+
+                        },
+                        Quantity = 2,
+                    },
+                },
+                Mode = "payment",
+                SuccessUrl = "https://localhost:44381/Home/Index",
+                CancelUrl = "https://localhost:44381/Home/About",
+            };
+            var services = new Stripe.Checkout.SessionService();
+            Stripe.Checkout.Session session = services.Create(options);
+            Response.Headers.Add("Location", session.Url);
+            return new HttpStatusCodeResult(303);
         }
 
         public ActionResult Contact()
