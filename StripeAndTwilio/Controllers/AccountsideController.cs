86894c732995d48ec6d52fffd656f8bb3a19da42
MirMoheen12 <91481488+MirMoheen12@users.noreply.github.com>
2023-02-28 20:53:01 +0500

    Add Login

---
+++ StripeAndTwilio/Controllers/AccountsideController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StripeAndTwilio.Controllers
+{
+    public class AccountsideController : Controller
+    {
+        // GET: Accountside
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
